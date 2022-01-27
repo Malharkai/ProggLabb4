66b490af3df49dab51d3d03541a27230d475e977
Malharkai <shotdragon96@hotmail.com>
2022-01-27 10:09:36 +0100

    Add files via upload

---
+++ ProgLabb4/Program.cs
@@ -0,0 +1,9 @@
+﻿using ProgLabb4;
+using System.Reflection;
+
+
+var list = WordList.GetLists();
+for (int i = 0; i < list.Length; i++)
+{
+    Console.WriteLine(list[i]);
+}
---
+++ ProgLabb4/WordList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgLabb4
+{
+    public class WordList
+    {
+        public static DirectoryInfo d = new DirectoryInfo(System.AppDomain.CurrentDomain.BaseDirectory + @"\Lists");
+        public string Name { get; }
+        public string[] Languages { get; }
+
+        public WordList(string name, params string[] languages)
+        {
+            name = Name;
+            languages = Languages;
+        }
+
+        public static string[] GetLists()
+        {
+            
+            FileInfo[] Files = d.GetFiles("*.dat"); 
+            List<string> list = new List<string>();
+
+            foreach (FileInfo file in Files)
+            {              
+                list.Add(Path.GetFileNameWithoutExtension(file.Name));
+            }
+            return list.ToArray();
+        }
+
+        public static WordList LoadList(string name)
+        {
+            name += ".dat";
+            FileInfo[] List = d.GetFiles(name);
+            List.
+
+        }
+    }
+}
